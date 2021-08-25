45389126db990987643a414138ca2ad909da5ffd
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-08-25 23:38:34 +0300

    moves normalize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
 import App from "./components/App/App";
-import "modern-normalize/modern-normalize.css";
 
 ReactDOM.render(
   <StrictMode>
